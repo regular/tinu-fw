68f776c058631d609ed15b05dffd051a2f2537f7
Jan Bölsche <regular.gonzales@googlemail.com>
2023-07-13 10:54:45 +0200

    Output EOT (ASCII 0x04) in a new line after giving a parsable reason for exiting

--- app/sysbios_bigtime_fw.cpp
+++ app/sysbios_bigtime_fw.cpp
@@ -13,18 +13,25 @@
 
 #include "Board.h"
 
+bool terminated = false;
 extern "C" void myExceptionHook(Hwi_ExcContext* ctx) {
-  System_printf("\nEOF EXCEPTION\n");
+  System_printf("\nEXCEPTION\x04");
+  System_flush();
+  terminated = true;
 }
 
 extern "C" void myAbortSystem() {
-  System_printf("\nEOF ABORT\n");
+  if (terminated) return System_abortStd();
+  terminated = true;
+  System_printf("\nABORT\x04");
   System_flush();
   System_abortStd();
 }
 
 extern "C" void myExitSystem(int code) {
-  System_printf("\nEOF EXIT: %d\n", code);
+  if (terminated) return System_exitStd(code);
+  terminated = true;
+  System_printf("\nEXIT %d\x04", code);
   System_flush();
   System_exitStd(code);
 }
