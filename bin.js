ae28aed177ffb9897f353a3e9da74cb2716196b7
Jan Bölsche <regular.gonzales@googlemail.com>
2023-07-14 10:59:50 +0200

    Additional .mak file created by js, allows to add external source files

--- bin.js
+++ bin.js
@@ -1,7 +1,9 @@
+const makefile = require('./lib/makefile')
 const make = require('./lib/make')
 
 async function main() {
   try {
+    makefile(['app/main.cpp', 'app/chiptypes.cpp'])
     await make()
   } catch(err) {
     console.error(err.message)
--- make.sh
+++ make.sh
@@ -1,9 +1,8 @@
 set -e
-
 source ~/.tisl/sdks/5.30.00.03.env # set by tinit
+NAME=${NAME:-tinu_fw}
 COMPILER=ccs # set by tinit
 RTOS=$TIRTOSNAME
 make -C app/$RTOS/$COMPILER
-
-OUT=app/$RTOS/$COMPILER/*.out
+OUT=app/$RTOS/$COMPILER/$NAME.out
 ls -l $OUT
